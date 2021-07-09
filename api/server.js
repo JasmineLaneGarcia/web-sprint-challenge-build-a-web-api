77240cafd1137326add3de7fc25575da04c2fd9f
Jasmine Lane-Garcia <jasminerlanegarcia@gmail.com>
2021-07-09 10:41:35 -0700

    Initial Commit

--- api/server.js
+++ api/server.js
@@ -6,4 +6,12 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+const actionsRouter = require('./actions/actions-router');
+
+const projectsRouter = require('./projects/projects-router');
+
+server.use(express.json());
+server.use(actionsRouter);
+server.use(projectsRouter);
+
 module.exports = server;
